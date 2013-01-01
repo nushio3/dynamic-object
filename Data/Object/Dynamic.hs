df31cf8a9eb8f3b93f10b01eaa81a2a6a81b65b8
Takayuki Muranushi <muranushi@gmail.com>
2013-01-01 11:41:03 +0900

    create first project.

---
+++ Data/Object/Dynamic.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE TypeFamilies #-}
+module Data.Object.Dynamic where
+
+import qualified Data.Map as Map
+import           Data.Dynamic
+import           Control.Lens
+
+newtype Object = Object (Map.Map TypeRep Dynamic)
+  deriving (Typeable)
+
+instance Show Object where
+  show (Object x) = show x
+
+class Typeable a => KeyType a where
+  type ValType a :: *
+
+type Record kt = Lens Object Object (Maybe (ValType kt)) (Maybe (ValType kt))
+
+empty :: Object
+empty = Object $ Map.empty
+
+mkRecord :: forall kt. (KeyType kt, Typeable (ValType kt)) => kt -> Record kt
+mkRecord k1 = lens gettr settr
+  where
+    gettr :: Object -> Maybe (ValType kt)
+    gettr (Object map0) = Map.lookup k map0 >>= fromDynamic
+    settr :: Object -> (Maybe (ValType kt)) -> Object
+    settr (Object map0) Nothing  = Object $ Map.delete k map0
+    settr (Object map0) (Just x) = Object $ Map.insert k (toDyn x) map0
+    k :: TypeRep
+    k = typeOf k1
