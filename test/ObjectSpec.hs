d02a09820afd1b17c55b11f34171d709b85ecce0
Takayuki Muranushi <muranushi@gmail.com>
2013-01-10 13:44:37 +0900

    add some test types.

--- test/ObjectSpec.hs
+++ test/ObjectSpec.hs
@@ -5,5 +5,14 @@ import Test.Hspec
 import Test.Hspec.QuickCheck (prop)
 import Test.QuickCheck
 
+import Data.Object.Dynamic
+import Data.Object.Dynamic.Presets
+
+
+newtype MassVelocity = MassVelocity { fromMassVelocity :: Object Precise }
+newtype MassMomentum = MassMomentum { fromMassMomentum :: Object Precise }
+
+
+
 spec :: Spec
 spec = return ()
