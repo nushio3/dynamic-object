ea3d97a1639753ab8b82ee74b7078ff3f9011258
Takayuki Muranushi <muranushi@gmail.com>
2013-01-25 21:53:04 +0900

    doctest discovery enabled.

--- Makefile
+++ Makefile
@@ -1,6 +1,16 @@
+all: build doc
+
+.PHONY: init build doc test
+
 init:
-	cabal install --enable-tests --only-dependencies
+	cabal install --only-dependencies --enable-tests
 	cabal configure --enable-tests
+
+build:
+	cabal build
+
+test: build
+	cabal test
+
 doc:
 	cabal haddock --hyperlink-source
-
--- test/doctests.hs
+++ test/doctests.hs
@@ -1,15 +1,29 @@
 module Main where
 
+import Control.Applicative
+import Control.Monad
 import Test.DocTest
+import System.Directory
+import System.FilePath
+
+findHs :: FilePath -> IO [FilePath]
+findHs dir = do
+  fs <- map (dir </>) <$>
+    filter (`notElem` ["..","."]) <$>
+    getDirectoryContents dir
+  subDirs <- filterM doesDirectoryExist fs
+  files1 <- filter ((`elem` [".hs", ".lhs"]) . takeExtension) <$>
+    filterM doesFileExist fs
+  files2 <- concat <$> mapM findHs subDirs
+  return $ files1 ++ files2
 
 main :: IO ()
-main = doctest 
-     [ "-XTypeFamilies"           
-     , "-XDeriveDataTypeable"           
-     , "-XFlexibleContexts"           
-     , "-XFlexibleInstances"           
-     , "Data/Object/Dynamic/Examples/PointParticle.hs"
-     , "Data/Object/Dynamic/Presets.hs"
-     , "Data/Object/Dynamic/Type.hs"
-     ]
-
+main = do
+  files <- findHs "Data"
+  putStrLn $ "testing: " ++ unwords files
+  doctest $
+    [ "-XTypeFamilies"
+    , "-XDeriveDataTypeable"
+    , "-XFlexibleContexts"
+    , "-XFlexibleInstances" ]
+    ++ files
